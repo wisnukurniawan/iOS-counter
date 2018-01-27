6915d48ce6103c14782503911b044f23a5b07d34
wisnu kurniawan <wisnuko13@gmail.com>
2018-01-27 08:58:12 +0700

    First init

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        countLabel.text = "0"
+        
+        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.onLongPressed(_:)))
+        longGesture.minimumPressDuration = 1
+        tapButton.addGestureRecognizer(longGesture)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +29,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onButtonTap(_ sender: UIButton) {
+        if let tempNum = Int(self.countLabel.text!) {
+            self.countLabel.text = "\(tempNum + 1)"
+        }
+    }
+    
+    @objc func onLongPressed(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .changed {
+            if let tempNum = Int(self.countLabel.text!) {
+                self.countLabel.text = "\(tempNum + 1)"
+            }
+        }
+    }
 
 }
 
